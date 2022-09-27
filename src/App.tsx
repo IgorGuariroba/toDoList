148efe660e0145cb565b5a20dd17a0f21a4ddc58
igor.guariroba <igorguariroba@movida.com.br>
2022-09-27 19:55:11 -0300

    Contabilizar tarefas

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,116 @@
-import { useState } from 'react'
+import { ChangeEvent, FormEvent, InvalidEvent, useState } from 'react'
 import { Header } from './Header'
 import { Plus } from "phosphor-react";
 
+import taskImg from './assets/Clipboard.png';
 import style from './App.module.css';
 import { Tasks } from './Tasks';
 
+interface Tasks {
+  id: string
+  content: string
+}
 
 function App() {
+
+  const [tasks, settasks] = useState([{
+    id: '',
+    content: ''
+  }
+  ])
+
+  const [task, setNewTask] = useState('');
+
+  function handleCreateNewTask(event: FormEvent) {
+    event.preventDefault();
+
+    settasks([...tasks, { id: task, content: task }]);
+    setNewTask('')
+  }
+
+  function handleNewTaskChange(event: ChangeEvent<HTMLInputElement>) {
+    event.target.setCustomValidity('')
+
+    setNewTask(event.target.value);
+  }
+
+  function handleNewTaskIvalid(event: InvalidEvent<HTMLInputElement>) {
+    event.target.setCustomValidity('Inclua um nova tafera')
+  }
+
+  function calcTasks() {
+    const countertask = tasks.reduce((total, task) => {
+      if (task.id != '') {
+        return total += 1;
+      }
+      return 0;
+    }, 0)
+
+    console.log(countertask);
+    return countertask;
+  }
+
   return (
     <div>
       <Header />
       <main>
-        <div className={style.search} >
-          <input type="text" />
-          <button className={style.criar}>
-            Criar
-            <Plus size={16} />
+        <form onSubmit={handleCreateNewTask}>
+          <div className={style.search} >
+            <input
+              placeholder='Adicione uma nova tarefa'
+              type="text"
+              value={task}
+              onChange={handleNewTaskChange}
+              onInvalid={handleNewTaskIvalid}
+              required
+            />
+
+            <button
+              type='submit'
+              className={style.criar}>
+              Criar
+              <Plus size={16} />
             </button>
-        </div>
+          </div>
+        </form>
 
-        <Tasks />
+        <article className={style.article}>
+          <div className={style.contentTarefa}>
+            <header className={style.info}>
+              <div>
+                <p className={style.created}>Tarefas criadas <span>5</span></p>
+              </div>
+              <div>
+                <p className={style.concluded}>Concluidas <span>2 de 5</span></p>
+              </div>
+            </header>
 
+            <div className={style.tasks}>
+              {
+                tasks.map(newtask => {
+                  if (newtask.id !== '') {
+                    return (<Tasks
+                      key={newtask.id}
+                      content={newtask.content}
+                      id={newtask.id}
+                    />)
+                  }
+                })
+              }
+
+              {
+                calcTasks() <= 0 ? <div className={style.zeroTask}>
+                  <img src={taskImg} alt="icon de tarefa" />
+                  <p>Você ainda não tem tarefas cadastradas </p>
+                  <p>Crie tarefas e organize seus itens a fazer</p>
+                </div> : ''
+              }
+
+            </div>
+
+          </div>
+
+        </article>
       </main>
 
     </div>
--- src/Tasks.tsx
+++ src/Tasks.tsx
@@ -1,45 +1,23 @@
 import { Trash } from 'phosphor-react';
 import style from './Tasks.module.css';
 
-import taskImg from './assets/Clipboard.png';
-import { useState } from 'react';
 
-export function Tasks() {
-  const [showElement, setShowElement] = useState(false)
+
+interface TaskProps {
+  id: string;
+  content: string;
+}
+export function Tasks({ content, id }: TaskProps) {
+  const istarefa = content != undefined;
+
   return (
-    <article className={style.article}>
-      <div className={style.content}>
-        <header className={style.info}>
-          <div>
-            <p className={style.created}>Tarefas criadas <span>5</span></p>
-          </div>
-          <div>
-            <p className={style.concluded}>Concluidas <span>2 de 5</span></p>
-          </div>
-        </header>
-
-        <div className={style.tasks}>
-          <section className={style.task}>
-            <div>
-              <input type="checkbox" name="" id="" />
-              <span className={style.input}>&#10004;</span>
-              <p>Teaste 123</p>
-            </div>
-            <button><Trash size={24} /></button>
-          </section>
-          {
-            showElement ? <div className={style.zeroTask}>
-              <img src={taskImg} alt="icon de tarefa" />
-              <p>Você ainda não tem tarefas cadastradas </p>
-              <p>Crie tarefas e organize seus itens a fazer</p>
-            </div> : ''
-          }
-
-
-        </div>
-
+    <section className={style.task}>
+      <div>
+        <input type="checkbox" name="" id={id} />
+        <span className={style.input}>&#10004;</span>
+        <p>{content}</p>
       </div>
-
-    </article>
+      <button><Trash size={24} /></button>
+    </section>
   )
 }
